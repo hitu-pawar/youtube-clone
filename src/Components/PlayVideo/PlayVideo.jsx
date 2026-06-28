336907355060f71f0e4a4046e6b47d36059691b2
Hitesh Pawar <hitupawar138@gmail.com>
2026-06-28 12:37:55 +0530

    Integrated YouTube Data API and dynamic feed

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PlayVideo.css';
 
 import video1 from '../../assets/video.mp4';
@@ -8,25 +8,48 @@ import share from '../../assets/share.png';
 import save from '../../assets/save.png';
 import jack from '../../assets/jack.png';
 import user_profile from '../../assets/user_profile.jpg';
+import { API_KEY, value_converter } from '../../data';
+import moment from 'moment'
 
-const PlayVideo = () => {
+const PlayVideo = ({videoId}) => {
+    
+  const [apiData,setApiData] = useState(null);
+
+  const fetchVideoData = async () =>{
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url).then(res=>res.json()).then(data =>setApiData(data.items[0]));
+  }
+
+  useEffect(()=>{
+    fetchVideoData();
+
+  },[])
+
   return (
     <div className="play-video">
 
-      <video src={video1} controls autoPlay muted></video>
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-      <h3>Best YouTube Channel To Learn Web Development</h3>
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
 
       <div className="play-video-info">
-        <p>1525 Views &bull; 2 days ago</p>
+        <p>
+  {apiData?.statistics?.viewCount
+    ? value_converter(apiData.statistics.viewCount)
+    : "16K"}{" "}
+  Views &bull;{" "}
+  {apiData?.snippet?.publishedAt
+    ? moment(apiData.snippet.publishedAt).fromNow()
+    : "2 days ago"}
+</p>
 
         <div>
           <span>
-            <img src={like} alt="" /> 125
+            <img src={like} alt="" /> {apiData?value_converter(apiData.statistics.likeCount):155}
           </span>
 
           <span>
-            <img src={dislike} alt="" /> 2
+            <img src={dislike} alt="" />
           </span>
 
           <span>
@@ -45,7 +68,7 @@ const PlayVideo = () => {
         <img src={jack} alt="" />
 
         <div>
-          <p>MernStack</p>
+          <p>{apiData?apiData.snippet.channelTitle:""}</p>
           <span>1M Subscribers</span>
         </div>
 
@@ -53,12 +76,11 @@ const PlayVideo = () => {
       </div>
 
       <div className="vid-description">
-        <p>Channel that makes learning easy.</p>
-        <p>Subscribe MernStack to watch more React and MERN tutorials.</p>
+        <p>{apiData?apiData.snippet.description:"Description Here"}</p>
 
         <hr />
 
-        <h4>130 Comments</h4>
+        <h4>{apiData?value_converter(apiData.statistics.commentCount):102} Comments</h4>
 
         <div className="comment">
           <img src={user_profile} alt="" />
